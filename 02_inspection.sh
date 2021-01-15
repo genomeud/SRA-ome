6bdf680360514985fa5357af01b8c683ee33701f
fabiomarroni <fabio.marroni@gmail.com>
2021-01-15 15:20:50 +0100

    Added shell scripts similar to pbs, to work on single node

--- 02_inspection.sh
+++ 02_inspection.sh
@@ -1,94 +1,5 @@
 #!/bin/bash
 
-
-
-
-################################################################################
-#
-# Metatranscriptomics analysis
-#
-################################################################################
-
-BASEDIR=/mnt/vol1/projects/SRAome
-DBDIR=$BASEDIR
-READDIR=${BASEDIR}/reads/Metatranscriptomics
-KRAKBIN=${BASEDIR}/kraken2/
-KDB=${BASEDIR}/database
-KRES=${BASEDIR}/classification/Metatranscriptomics
-LOGDIR=${BASEDIR}/logs
-
-module load it/lang/r/3.6.1
-#Needed only once
-# Rscript ${BASEDIR}/functions/01_explore_SRA.r \
-# -D FALSE \
-# -S SRAmetadb.sqlite \
-# -F $DBDIR \
-# -T 'metatranscriptomics'
-
-#Needed recursively (if we increase D we increase the number of SRA experiments downloaded in a single batch!)
-#From newer to older
-Rscript ${BASEDIR}/02_download_metrans_SRA.r \
--S ${DBDIR}/SRAmetadb.sqlite \
--F $READDIR \
--R TRUE \
--D 30 \
--I ${DBDIR}/metatranscriptomics_table.txt 
-
-myd_for=`echo "module load it/lang/r/3.6.1; Rscript /projects/populus/ep/share/marroni/functions/EOSC/02_download_metrans_SRA.r \
--S ${DBDIR}/SRAmetadb.sqlite \
--F $READDIR \
--R FALSE \
--D 30 \
--I ${DBDIR}/metatranscriptomics_table.txt \
->${LOGDIR}/d_for.out 2>${LOGDIR}/d_for.err"| qsub -N df_for -l vmem=15G,walltime=168:00:00,nodes=1:ppn=1 -W depend=afterany:$myd_rev` 
-
-#Needed recursively (on stratocluster) (still need to deal with discriminating between single and paired reads)
-#Right now, only single reads
-#I decided that I don't need the analysis on the full nt anymore. Better to focus on the minikraken database
-#To run in screen we need this compiler, I don't know why!!
-compilers/gcc/5.4.0_novabreed
-NPROCS=8
-${BASEDIR}/scripts/02_class_metag.sh $KRAKBIN $KDB $KRES $READDIR $LOGDIR $NPROCS
-
-
-
-#mykmini=`qsub -F "$MINIKDB $MINIKRES $READDIR" /projects/populus/ep/share/marroni/scripts/EOSC/01_class_metag.pbs -W depend=afterany:$myd_for:$myd_rev` 
-#Only execute if everything is fine
-echo "rm ${KRES}/*.kraken; rm ${MINIKRES}/*.kraken; rm $READDIR/*.gz" | qsub -N myc -l vmem=2G,walltime=1:00:00,nodes=1:ppn=1 -W depend=afterok:$mykmini
-#Script for moving files to subfolders according to starting characters
-qsub -F "$MINIKRES SRR 4" /projects/populus/ep/share/marroni/scripts/EOSC/03_move_kres.pbs -W depend=afterok:$mykmini 
-qsub -F "$MINIKRES ERR 4" /projects/populus/ep/share/marroni/scripts/EOSC/03_move_kres.pbs -W depend=afterok:$mykmini 
-qsub -F "$MINIKRES DRR 4" /projects/populus/ep/share/marroni/scripts/EOSC/03_move_kres.pbs -W depend=afterok:$mykmini 
-
-
-
-
-#To check the progress quickly!
-#cat d_rev.err| grep -v "\\.\\.\\.\\." | less
-#Check how many donwload were completed or attempted
-cut -f43  ${DBDIR}/metatranscriptomics_table.txt | sort | uniq -c
-#Check how many metagenomics results file we have
-ls -l $MINIKRES/*/ | wc -l
-#ls -l $MINIKRES | wc -l
-for aaa in ${MINIKRES}/*
-do
-echo $aaa
-ls -l $aaa | wc -l 
-done
-
-
-
-
-#This is my horrible R function to create a unique huge table with all results. I will just need to run it every now and then
-# Rscript /projects/populus/ep/share/marroni/functions/EOSC/03_summarize_K.r \
-# -I $MINIKRES \
-# -P kraken.report.txt \
-# -O /projects/populus/ep/share/marroni/EOSC/output/Metagenomics_mini/Summary.txt
-
-
-
-
-
 ################################################################################
 #
 # Metagenomics analysis
@@ -101,16 +12,25 @@ KRAKBIN=/projects/populus/ep/software/kraken2-2.0.6-beta/bin
 KDB=/projects/igats/metagenomics/custom_databases/kraken_nt_2.0.7
 MINIKDB=/projects/populus/ep/share/marroni/databases/minikraken2_v2_8GB_201904_UPDATE
 KRES=/projects/populus/ep/share/marroni/EOSC/classification/Metagenomics
-MINIKRES=/projects/populus/ep/share/marroni/EOSC/classification/Metagenomics_mini
 LOGDIR=/projects/populus/ep/share/marroni/scripts/logs
 
-module load it/lang/r/3.6.1
-#Needed only once
-# Rscript /projects/populus/ep/share/marroni/functions/EOSC/01_explore_SRA.r \
-# -D FALSE \
-# -S SRAmetadb.sqlite \
-# -F $DBDIR \
-# -T 'metagenomics'
+
+
+#Search sars-cov-2 quickly
+MINIKRES=/projects/populus/ep/share/marroni/EOSC/classification/Metagenomics_mini
+MINIKRES=/projects/populus/ep/share/marroni/EOSC/classification/Metatranscriptomics
+awk '$5==2697049' ${MINIKRES}/*/*.kraken.report.txt
+
+
+
+
+
+
+#######################
+#
+#OLD STUFF BELOW!!!
+#
+#######################
 
 cd $LOGDIR
 #Needed recursively (if we increase D we increase the number of SRA experiments downloaded in a single batch!)
@@ -135,7 +55,7 @@ myd_for=`echo "module load it/lang/r/3.6.1; Rscript /projects/populus/ep/share/m
 #Right now, only single reads
 #I decided that I don't need the analysis on the full nt anymore. Better to focus on the minikraken database
 #myk=`qsub -F "$KDB $KRES" /projects/populus/ep/share/marroni/scripts/EOSC/01_class_metag.pbs -W depend=afterany:$myd` 
-mykmini=`qsub -F "$MINIKDB $MINIKRES $READDIR" /projects/populus/ep/share/marroni/scripts/EOSC/01_class_metag.pbs -W depend=afterany:$myd_for:$myd_rev` 
+mykmini=`qsub -F "$MINIKDB $MINIKRES" /projects/populus/ep/share/marroni/scripts/EOSC/01_class_metag.pbs -W depend=afterany:$myd_for:$myd_rev` 
 #Only execute if everything is fine
 echo "rm ${KRES}/*.kraken; rm ${MINIKRES}/*.kraken; rm $READDIR/*.gz" | qsub -N myc -l vmem=2G,walltime=1:00:00,nodes=1:ppn=1 -W depend=afterok:$mykmini
 #Script for moving files to subfolders according to starting characters
@@ -166,7 +86,6 @@ Rscript /projects/populus/ep/share/marroni/functions/EOSC/03_summarize_K.r \
 -I $MINIKRES \
 -P kraken.report.txt \
 -O /projects/populus/ep/share/marroni/EOSC/output/Metagenomics_mini/Summary.txt
-
 
 
 
