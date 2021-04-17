0913bf2b18c1d51c4f6901e636afa93dcf5ada5f
zucchi99 <francesco.zuccato99@gmail.com>
2021-04-17 12:36:47 +0200

    script che esegue TUTTO

---
+++ scripts/god.sh
@@ -0,0 +1,89 @@
+#set -x
+
+#output directories
+#newmainOutDir='/mnt/extra/fzuccato_new/'$(date +'%Y-%m-%g')
+mainOutDir='/mnt/extra/fzuccato_new/2021_04_18/'
+infoOutDir="$mainOutDir"'.info/'
+researchesOutDir="$mainOutDir"'.researches/'
+
+#input directories
+mainInDir=$HOME'/SRA/'
+scriptsInDir=$mainInDir'scripts/'
+metadataInDir=$mainInDir'metadata/'
+
+#input and output files
+metadataToDo_file=$metadataInDir'metadata_filtered_small_todo.csv'
+metadataAll_file=$metadataInDir'metadata_filtered_small.csv'
+runsToDo_file=$infoOutDir'runs_list.csv'
+
+#scripts
+pickRandom_script=$scriptsInDir'pickRandomInPercentage.sh'
+execFasterQKrakenUpdate_script=$scriptsInDir'execFasterQ_Kraken_update.sh'
+searchInAnalysis_script=$scriptsInDir'searchInAnalysis.sh'
+
+if test -d $mainOutDir
+then
+    echo "error: directory $mainOutDir already exists"
+    exit 1
+else
+    mkdir $mainOutDir
+    if test $? -ne 0
+    then
+        echo "error: can't make directory $mainOutDir"
+        exit 2
+    fi
+fi
+
+#1) pick random 
+
+#   a) parameters:
+scientificName_idx=9 
+run_idx=8 
+layout_idx=14 
+sizeMB_idx=17 
+
+#   b) execution
+pickRnd_cmd="$pickRandom_script \
+    $metadataToDo_file \
+    $scientificName_idx \
+    $run_idx \
+    $layout_idx \
+    $sizeMB_idx \
+    $infoOutDir"
+
+echo $pickRnd_cmd
+#$pickRnd_cmd
+
+#2,3,4) execFasterQ_Kraken_update
+#   a) parameters
+createInfoFile_boolean='FALSE'
+
+#   b) execution
+execAll_cmd="$execFasterQKrakenUpdate_script \
+    $runsToDo_file \
+    $mainOutDir \
+    $createInfoFile_boolean \
+    $metadataAll_file"
+
+echo $execAll_cmd
+#$execAll_cmd
+
+#5) searchInAnalysis: corona, coronavirus, sars-cov-2
+
+fieldIdxs_array=('6' '6' '5')
+valueIdxs_array=('corona' 'coronavirus' '2697049')
+
+n=${#fieldIdxs_array[@]}
+
+for (( i=0; i<$n; i++ ))
+do
+    execSearch_cmd="$searchInAnalysis_script \
+        $mainOutDir \
+        ${fieldIdxs_array[$i]} \
+        ${valueIdxs_array[$i]} \
+        $researchesOutDir"
+
+    echo $execSearch_cmd
+    $execSearch_cmd
+
+done
