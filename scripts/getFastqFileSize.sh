2502b7cb3e5af31d355580999f6f919718093e5a
zucchi99 <francesco.zuccato99@gmail.com>
2021-04-30 09:57:14 +0200

    script aggiornato a prova di SRA stupido :)

--- scripts/getFastqFileSize.sh
+++ scripts/getFastqFileSize.sh
@@ -13,9 +13,9 @@ layout=$3
 
 cd $folderSearch
 
-sizesB=`ls -al | tr -s ' ' | egrep "$run(_[1,2])?\.fastq" | cut -d' ' -f5`
+sizesB_array=(`ls -al | tr -s ' ' | egrep "$run(_[1,2])?\.fastq" | cut -d' ' -f5`)
 
-if test -z "$sizesB"
+if test ${#sizesB_array[@]} -eq 0
 then
     echo "Error: $0 did not found any fastq file"
     exit 2
@@ -25,17 +25,32 @@ totalSizeMB=0
 #B_to_MB=$((2 ** 20)) #gives error if called from c++
 B_to_MB=`echo "2 20" | awk '{ print ($1 ^ $2); }'`
 
-if test $layout = 'SINGLE'
+if test ${#sizesB_array[@]} -eq 1
 then
-    fastqSizeB=`echo $sizesB`
+    if ! test $layout = 'SINGLE'
+    then
+        echo "Warning: $0 found one fastq file of $run but run layout is not SINGLE, is there an error in SRA db?"
+    fi
+    fastqSizeB=${sizesB_array[0]}
     fastqSizeMB=$(( $fastqSizeB / $B_to_MB ))
     totalSizeMB=$fastqSizeMB
-else
-    fastq1SizeB=`echo $sizesB | cut -d' ' -f1`
-    fastq2SizeB=`echo $sizesB | cut -d' ' -f2`
+    
+elif test ${#sizesB_array[@]} -eq 2
+then
+    if ! test $layout = 'PAIRED'
+    then
+        echo "Warning: $0 found two fastq file of $run but run layout is not PAIRED, is there an error in SRA db?"
+    fi
+    fastq1SizeB=${sizesB_array[0]}
+    fastq2SizeB=${sizesB_array[1]}
     fastq1SizeMB=$(( $fastq1SizeB / $B_to_MB )) 
     fastq2SizeMB=$(( $fastq2SizeB / $B_to_MB ))
     totalSizeMB=$(( $fastq1SizeMB + $fastq2SizeMB ))
+else
+    echo "Error: $0 found ${#sizesB_array[@]} fastq files of $run $layout, this was not expected."
+    array=(`ls -al | tr -s ' ' | egrep "$run(_[1,2])?\.fastq"`)
+    echo "List of fastq files found:" ${array[@]:0:${#array[@]}}
+    exit 3
 fi
 
 echo $totalSizeMB
