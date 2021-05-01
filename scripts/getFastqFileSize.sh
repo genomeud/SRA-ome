af42ff951fcbc885f9319cc942e8b77d4ad03f05
zucchi99 <francesco.zuccato99@gmail.com>
2021-05-01 09:11:17 +0200

    modificati valori per gli exit status

--- scripts/getFastqFileSize.sh
+++ scripts/getFastqFileSize.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #set -x
 
-nOfParamsNeeded=3
+nOfParamsNeeded=2
 if test $# -lt $nOfParamsNeeded
 then
-    echo "usage: $0 <folderSearch> <run> <layout = SINGLE | PAIRED>"
-    exit -1
+    echo "usage: $0 <folderSearch> <run> [<layout = SINGLE | PAIRED>]"
+    exit 1
 fi
 
 #NB, in this script i use:
-#exit status < 0 ==> ERROR
-#exit status > 0 ==> WARNING
+#exit status  1-99 ==> ERROR
+#exit status  100+ ==> WARNING
 
 folderSearch=$1
 run=$2
@@ -18,49 +18,47 @@ layout=$3
 
 cd $folderSearch
 
-sizesB_array=(`ls -al | tr -s ' ' | egrep "$run(_[1,2])?\.fastq" | cut -d' ' -f5`)
+files=(`ls -al | tr -s ' ' | egrep "$run.*"'.fastq' | cut -d' ' -f9`)
+sizesB_array=(`ls -al | tr -s ' ' | egrep "$run.*"'.fastq' | cut -d' ' -f5`)
+#sizesB_array=(`ls -al | tr -s ' ' | egrep "$run(_[1,2])?\.fastq" | cut -d' ' -f5`)
+n=${#sizesB_array[@]}
 
-if test ${#sizesB_array[@]} -eq 0
+if test $n -eq 0
 then
     echo "Error: $0 did not found any fastq file"
-    exit -2
+    exit 2
 fi
 
 totalSizeMB=0
 #B_to_MB=$((2 ** 20)) #gives error if called from c++
 B_to_MB=`echo "2 20" | awk '{ print ($1 ^ $2); }'`
 
+totalSizeMB=0
+
+for (( i=0; i<$n; i++ ))
+do
+    fastqSizeMB=$(( ${sizesB_array[$i]} / $B_to_MB ))
+    totalSizeMB=$(( $totalSizeMB + $fastqSizeMB ))
+done
+
+#check integrity and correctness of SRA db
 warningMessage=''
 warningStatus=0
-
-if test ${#sizesB_array[@]} -eq 1
+if test \( $n -eq 1 \) -a \( $layout = 'PAIRED' \)
 then
-    if ! test $layout = 'SINGLE'
-    then
-        warningMessage="Warning: $0 found one fastq file of $run but run layout is not SINGLE, is there an error in SRA db?"
-        warningStatus=1
-    fi
-    fastqSizeB=${sizesB_array[0]}
-    fastqSizeMB=$(( $fastqSizeB / $B_to_MB ))
-    totalSizeMB=$fastqSizeMB
+    warningMessage="warning: $0 found one fastq file of $run but run layout is not SINGLE, is there an error in SRA db?"
+    warningStatus=1
     
-elif test ${#sizesB_array[@]} -eq 2
+elif test \( $n -eq 2 \) -a \( $layout = 'SINGLE' \)
 then
-    if ! test $layout = 'PAIRED'
-    then
-        warningMessage="Warning: $0 found two fastq file of $run but run layout is not PAIRED, is there an error in SRA db?"
-        warningStatus=2
-    fi
-    fastq1SizeB=${sizesB_array[0]}
-    fastq2SizeB=${sizesB_array[1]}
-    fastq1SizeMB=$(( $fastq1SizeB / $B_to_MB )) 
-    fastq2SizeMB=$(( $fastq2SizeB / $B_to_MB ))
-    totalSizeMB=$(( $fastq1SizeMB + $fastq2SizeMB ))
-else
-    echo "Error: $0 found ${#sizesB_array[@]} fastq files of $run $layout, this was not expected."
-    array=(`ls -al | tr -s ' ' | egrep "$run(_[1,2])?\.fastq"`)
-    echo "List of fastq files found:" ${array[@]:0:${#array[@]}}
-    exit -3
+    warningMessage="warning: $0 found two fastq file of $run but run layout is not PAIRED, is there an error in SRA db?"
+    warningStatus=2
+elif test $n -gt 2
+then
+    #error
+    warningMessage="warning: $0 found ${#sizesB_array[@]} fastq files of $run $layout, this was not expected. "
+    warningMessage="$warningMessage""List of fastq files found: ${files[@]:0:${#files[@]}}"
+    warningStatus=3
 fi
 
 #print output
@@ -70,7 +68,7 @@ echo $totalSizeMB
 if test $warningStatus -ne 0
 then
     echo "$warningMessage"
-    exit $warningStatus
+    exit $((100 + $warningStatus))
 fi
 
 exit 0
