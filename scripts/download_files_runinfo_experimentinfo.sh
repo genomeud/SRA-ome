377f31e9f8937a9156f3090fbcb3fa8f464b8276
zucchi99 <francesco.zuccato99@gmail.com>
2021-07-14 13:41:20 +0200

    aggiunta formattazione

--- scripts/download_files_runinfo_experimentinfo.sh
+++ scripts/download_files_runinfo_experimentinfo.sh
@@ -3,9 +3,17 @@
 #rettype=runinfo ==> tabella csv: ogni riga le info di una run
 #SOURCE = METATRANSCRIPTOMIC ==> solo metatranscrittomica
 #circa 20-25 MB di file
-wget 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term="METATRANSCRIPTOMIC"[Source]'
+wget -O run_info.csv 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term="METATRANSCRIPTOMIC"[Source]'
 
 #rettype=experimentinfo ==> file xml con tutti i metadati: esperimenti, studi, samples, run...
 #SOURCE = METATRANSCRIPTOMIC ==> solo metatranscrittomica
+#NB: il file generato è xml ma tutto in una riga, va formattato (ad esempio con xmllint)
 #circa 400-450 MB di file
-wget 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=experimentinfo&term="METATRANSCRIPTOMIC"[Source]'
+wget  -O experiment_info.xml 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=experimentinfo&term="METATRANSCRIPTOMIC"[Source]'
+
+#format file
+cat experiment_info.xml \
+| xmllint --format - \
+>experiment_info_formatted.xml
+
+#rm experiment_info.xml
