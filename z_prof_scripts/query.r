9d83e02241fc56088e036de2a49d14dc7a4095d0
zucchi99 <francesco.zuccato99@gmail.com>
2021-10-08 11:03:34 +0200

    no more useful old code

--- z_prof_scripts/query.r
+++
@@ -1,7 +0,0 @@
-#library_strategy LIKE 'RNA-seq' AND 
-#library_source LIKE 'METATRANSCRIPTOMIC'"
-onlyq <- dbGetQuery(
-    con, paste(
-        "SELECT * 
-        FROM experiment"
-    , sep=""));
