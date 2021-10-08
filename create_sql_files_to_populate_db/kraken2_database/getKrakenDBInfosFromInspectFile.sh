cf4ddd0e500108bcab819003d526a3265282d02a
zucchi99 <francesco.zuccato99@gmail.com>
2021-10-08 17:37:19 +0200

    modifiche krakenrecord

--- create_sql_files_to_populate_db/kraken2_database/getKrakenDBInfosFromInspectFile.sh
+++ create_sql_files_to_populate_db/kraken2_database/getKrakenDBInfosFromInspectFile.sh
@@ -1,12 +1,11 @@
 #set -x
 
 nOfParamsNeeded=3
-
 if test $# -ne $nOfParamsNeeded
 then
     echo "usage: $0 <inspect.txt> <collection_name> <collection_date>"
-    echo 'example:' $0 'Kraken2_DB16_inspect.txt' 'Standard-16' '2020-02-12'
-    echo 'output_file: Kraken2_DB16_<collection_name>_<collection_date>.sql'
+    echo 'example:' $0 'inspect.txt' 'Standard-16' '2020-12-02'
+    echo 'output_file: 3_kdb_KrakenRecord_<collection_name>_<collection_date>.sql'
     exit 1
 fi
 
@@ -14,7 +13,7 @@ krakendb_if=$1
 collection=$2
 collection_date=$3
 
-output_file='Kraken2_DB16_'$collection'_'$collection_date'.sql'
+output_file='3_kdb_KrakenRecord_'$collection'_'$collection_date'.sql'
 temp_file='tmp'
 #fields:
 #PercFrags, RootedFrags, DirectFrags, Rank, TaxId, ScientificName
